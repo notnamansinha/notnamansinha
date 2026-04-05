c3d0725cee80010465cf9d2180e726b55f48c040
naman <notnamansinha@gmail.com>
2026-04-06 02:23:24 +0530

    fix: revert force_cache, let action naturally rebuild LOC from scratch

--- today.py
+++ today.py
@@ -424,7 +424,7 @@ if __name__ == '__main__':
     # Birthday: Update this to your actual date of birth
     age_data, age_time = perf_counter(daily_readme, datetime.datetime(2006, 1, 5))
     formatter('age calculation', age_time)
-    total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 0, True)
+    total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 0)
     formatter('LOC (cached)', loc_time) if total_loc[-1] else formatter('LOC (no cache)', loc_time)
     commit_data, commit_time = perf_counter(commit_counter, 0)
     star_data, star_time = perf_counter(graph_repos_stars, 'stars', ['OWNER'])
